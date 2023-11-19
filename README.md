d3a6f0d107ff1dd8c4f1663aaee57ad7eb190796
Alal Muhammad <ryuzakinote91@gmail.com>
2023-11-19 15:04:52 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# renative_initial
---
+++ src/entry/index.lng.ts
@@ -0,0 +1,7 @@
+//@ts-nocheck
+import { Launch } from '@lightningjs/sdk';
+import App from '../app';
+
+const Main = (...args) => Launch(App, ...args);
+
+export default Main;
